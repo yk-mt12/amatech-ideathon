7ab61de0485e1dec2cfcc47c31ade3eec6d32311
kitsune_yk <agvksl543@gmail.com>
2023-03-19 11:14:22 +0900

    chore: install tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  mode: 'jit',
+  darkMode: false, // 'media' or 'class',
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
