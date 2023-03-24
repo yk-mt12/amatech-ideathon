737ec63eb994b55ada72af9d1eda6fcd59899f74
kitsune_yk <agvksl543@gmail.com>
2023-03-25 08:04:41 +0900

    twitterリンクを修正

--- src/components/Twitter.tsx
+++ src/components/Twitter.tsx
@@ -8,19 +8,26 @@ export const Twitter = () => {
         イベントの様子や開催中の雰囲気などを発信します！スポンサー企業のご紹介もお楽しみに！
         <br />
         当日のハッシュタグ
-        <span className="text-1xl font-bold text-blue-400">#あまてくアイディアソン</span>
+        <span className="text-1xl font-bold text-blue-400 hover:text-blue-500">
+          <a
+            href="https://twitter.com/intent/tweet?hashtags=あまてくアイディアソン"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+          #あまてくアイディアソン
+        </span>
         <br />
         学んだことや感想などをツイートしてください！
       </p>
       <button className="mt-4 rounded-full bg-blue-400 py-2 px-4 hover:bg-blue-500">
         <a
-          href="https://twitter.com/intent/tweet?hashtags=あまてくアイディアソン"
+          href="https://twitter.com/amatech1006"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center justify-center rounded-full py-2 px-4  font-bold text-white  "
         >
           <FaTwitter className="mr-2" />
-          ツイートする
+          Twitterを見る
         </a>
       </button>
     </div>
