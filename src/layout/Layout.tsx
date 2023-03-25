7cf37aa5988e4baa9a71cff490994236a61469f1
kitsune_yk <agvksl543@gmail.com>
2023-03-25 17:05:50 +0900

    ナビを修正

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -18,42 +18,43 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="bg-slate-100">
       <nav className="bg-white shadow">
-        <div className="container mx-auto px-6 py-4 md:flex md:justify-between md:items-center">
-          <div className="flex justify-between items-center">
+        <div className="container mx-auto px-6 py-4 md:flex md:items-center md:justify-between">
+          <div className="flex items-center justify-between">
             <h1 className="text-xl font-semibold text-gray-800 lg:text-2xl">
               <Link href="/" className="flex items-center">
-                  <Image
-                    className="inline-block h-8 w-8"
-                    src="/images/logo/amatech-logo.jpg"
-                    alt="あまてくロゴ"
-                    width={256}
-                    height={256}
-                  />
-                  <span className="ml-2">あまてく アイディアソン</span>
+                <Image
+                  className="inline-block h-8 w-8"
+                  src="/images/logo/amatech-logo.jpg"
+                  alt="あまてくロゴ"
+                  width={256}
+                  height={256}
+                />
+                <span className="ml-2">あまてく アイディアソン</span>
               </Link>
             </h1>
-            <div className="md:hidden">
+            <div className="flex flex-col items-center justify-center text-gray-800 md:hidden top-50">
               <button
                 onClick={toggleMenu}
                 type="button"
-                className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
-                aria-label="Toggle menu"
+                className="z-10 space-y-2"
               >
-                <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-                  {menuOpen ? (
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M18 6L6 18M6 6l12 12"
-                    />
-                  ) : (
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  )}
-                </svg>
+                <div
+                  className={
+                    menuOpen
+                      ? "h-0.5 w-8 translate-y-2.5 rotate-45 bg-gray-600"
+                      : "h-0.5 w-8 bg-gray-600"
+                  }
+                />
+                <div
+                  className={menuOpen ? "opacity-0" : "h-0.5 w-8 bg-gray-600"}
+                />
+                <div
+                  className={
+                    menuOpen
+                      ? "h-0.5 w-8 -rotate-45 bg-gray-600"
+                      : "h-0.5 w-8 bg-gray-600"
+                  }
+                />
               </button>
             </div>
           </div>
@@ -63,12 +64,18 @@ const Layout = ({ children }: Props) => {
               menuOpen ? "block" : "hidden"
             } md:flex md:items-center md:justify-between `}
           >
-            <div className="flex flex-col md:flex-row md:items-center">
-              <Link href="/vol1" className="my-1 md:my-0 md:mx-4 text-gray-800 font-bold hover:text-blue-500">
-                  Vol1
+            <div className="flex flex-col md:flex-row md:items-center mt-5">
+              <Link
+                href="/vol1"
+                className="my-1 font-bold text-gray-800 hover:text-blue-500 md:my-0 md:mx-4 text-right"
+              >
+                Vol1
               </Link>
-              <Link href="/Contact" className="my-1 md:my-0 md:mx-4 text-gray-800 font-bold hover:text-blue-500">
-                  Contact
+              <Link
+                href="/Contact"
+                className="my-1 font-bold text-gray-800 hover:text-blue-500 md:my-0 md:mx-4 text-right"
+              >
+                Contact
               </Link>
             </div>
           </div>
