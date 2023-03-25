383d1e4486cf3c6f24c248a3bbd5165314da48cc
kitsune_yk <agvksl543@gmail.com>
2023-03-25 09:16:33 +0900

    seoを修正7

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Seo from "@/layout/Seo";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="ja"  prefix="og: http://ogp.me/ns#">
+    <Html lang="ja" prefix="og: http://ogp.me/ns#">
       <Head />
       <body>
+      <Seo />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
